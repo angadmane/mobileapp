32898e645ab3d7b2f2cf6863f3e37637b35a2697
jiteshmegeri <surjitmegeri@gmail.com>
2024-08-01 16:39:45 +0530

    gps change

--- app/(tabs)/GPS.js
+++ app/(tabs)/GPS.js
@@ -1,12 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const chats = () => {
   return (
     <View>
       <Text>chats</Text>
+      <Text>jitesh</Text>
     </View>
-  )
-}
+  );
+};
 
-export default chats
+export default chats;
