a534159dd9c0b148f632853e75163cc763f3ffc8
jiteshmegeri <surjitmegeri@gmail.com>
2024-08-01 16:59:10 +0530

    j changes

---
+++ app/(tabs)/GPS.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const chats = () => {
+  return (
+    <View>
+      <Text>chats</Text>
+      <Text>jitesh</Text>
+      <Text>kashinath is back</Text>
+    </View>
+  );
+};
+
+export default chats;
