67ed6b66708d7626e7a0ae9afe82e1d5f5a5d981
Ashish-Wangi <ashish.wangi@hotmail.com>
2024-08-02 11:09:13 +0530

    Test

---
+++ app/(tabs)/(GPS)/AIS140.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AIS140() {
+  return (
+    <View>
+      <Text>AIS140</Text>
+    </View>
+  )
+}
--- app/(tabs)/GPS.js
+++ app/(tabs)/GPS.js
@@ -1,12 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Touchable,
+} from "react-native";
+import React from "react";
 
-const chats = () => {
+const GPS = () => {
+  const pressHandler = () => {
+    console.log("Via Phone ");
+    navigation.navigate("(GPS)");
+  };
   return (
-    <View>
-      <Text>chats</Text>
-    </View>
-  )
-}
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.tile} onPress={pressHandler}>
+        <Image
+          source={require("../../assets/AIS140.png")}
+          style={styles.gpsImage}
+        />
+        <View>
+          <Text>AIS 140</Text>
+        </View>
+      </TouchableOpacity>
 
-export default chats
+      <View style={styles.tile}>
+        <Image
+          source={require("../../assets/AIS140.png")}
+          style={styles.gpsImage}
+        />
+        <Text>Non-AIS</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "Top",
+    height: "100%",
+  },
+  tile: {
+    width: "40%",
+    height: "20%",
+    backgroundColor: "#f3f7ff",
+    justifyContent: "center",
+    alignItems: "center",
+    //borderWidth: 1,
+    margin: "5%",
+    borderColor: "black",
+    borderRadius: 15,
+  },
+  gpsImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+});
+
+export default GPS;
