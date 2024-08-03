b309c2c13ea3a90bf20a0dac99979dcfbbc03955
jiteshmegeri <surjitmegeri@gmail.com>
2024-08-03 11:04:51 +0530

    jitesh changes

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,21 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
-import { Header } from 'react-native/Libraries/NewAppScreen'
+import { View, Text } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 const _layout = () => {
   return (
     <Stack>
-        <Stack.Screen name='index' options={{contentStyle:{backgroundColor:'#EFFFFF'}}} />
-        <Stack.Screen name='(tabs)' options={{headerShown:false}} />
+      <Stack.Screen
+        name="index"
+        options={{
+          contentStyle: { backgroundColor: "#EFFFFF" },
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
-  )
-}
+  );
+};
 
-export default _layout
+export default _layout;
